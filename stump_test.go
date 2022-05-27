e3b3a5a10d5970530ea64844592e696f62b47667
Calvin Kim <calvin@kcalvinalvin.info>
2022-05-27 17:19:38 +0900

    stump_test: Add tests for stump
    FuzzStump and FuzzStumpChain are added. Like the existing tests such as
    FuzzPollardModify and FuzzPollardModifyChain, the basic idea is the
    same.
    
    FuzzStump does 2 modifications on the stump and is more beneficial
    during debugging as you can limit the amount of leaves, making it easier
    to visualize.
    
    FuzzStumpChain uses a simulation blockchain to tests for
    errors. It's much harder to visualize but having more modifications
    allows for better testing of Stump.

---
+++ stump_test.go
@@ -0,0 +1,182 @@
+package utreexo
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func FuzzStump(f *testing.F) {
+	var tests = []struct {
+		startLeaves uint32
+		modifyAdds  uint32
+		delCount    uint32
+		seed        int64
+	}{
+		{
+			8,
+			2,
+			3,
+			0,
+		},
+		{
+			6,
+			2,
+			5,
+			0,
+		},
+		{
+			16,
+			4,
+			10,
+			0,
+		},
+		{
+			16,
+			6,
+			14,
+			424,
+		},
+	}
+	for _, test := range tests {
+		f.Add(test.startLeaves, test.modifyAdds, test.delCount, test.seed)
+	}
+
+	f.Fuzz(func(t *testing.T, startLeaves uint32, modifyAdds uint32, delCount uint32, seed int64) {
+		// Set seed to make sure the test is reproducible.
+		rand.Seed(seed)
+
+		// delCount must be less than the current number of leaves.
+		if delCount > startLeaves {
+			return
+		}
+
+		p := NewAccumulator(true)
+		stump := Stump{}
+
+		leaves, delHashes, _ := getAddsAndDels(uint32(p.numLeaves), startLeaves, delCount)
+		err := p.Modify(leaves, nil, nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		adds := make([]Hash, len(leaves))
+		for i := range leaves {
+			adds[i] = leaves[i].Hash
+		}
+
+		stump, err = UpdateStump(nil, adds, Proof{}, stump)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Check that the roots are the same after the addition.
+		if len(p.roots) != len(stump.Roots) {
+			t.Fatalf("FuzzStump fail: jHave %d roots for pollard and %d roots for stump."+
+				"\nStump:\n%s\nPollard:\n%s\n", len(p.roots), len(stump.Roots),
+				printHashes(stump.Roots), printPolNodes(p.roots))
+		}
+		for i := range p.roots {
+			if p.roots[i].data != stump.Roots[i] {
+				t.Fatalf("FuzzStump fail: Roots do not equal between pollard and stump."+
+					"\nStump:\n%s\nPollard:\n%s\n%s\n",
+					printHashes(stump.Roots), printPolNodes(p.roots), p.String())
+			}
+		}
+
+		proof, err := p.Prove(delHashes)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		modifyLeaves, _, _ := getAddsAndDels(uint32(p.numLeaves), 0, 0)
+		err = p.Modify(modifyLeaves, delHashes, proof.Targets)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		adds = make([]Hash, len(modifyLeaves))
+		for i := range adds {
+			adds[i] = modifyLeaves[i].Hash
+		}
+
+		stump, err = UpdateStump(delHashes, adds, proof, stump)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// Check that the roots are the same after addition/deletion.
+		if len(p.roots) != len(stump.Roots) {
+			t.Fatalf("FuzzStump fail: jHave %d roots for pollard and %d roots for stump."+
+				"\nStump:\n%s\nPollard:\n%s\n", len(p.roots), len(stump.Roots),
+				printHashes(stump.Roots), printPolNodes(p.roots))
+		}
+		for i := range p.roots {
+			if p.roots[i].data != stump.Roots[i] {
+				t.Fatalf("FuzzStump fail: Roots do not equal between pollard and stump."+
+					"\nStump:\n%s\nPollard:\n%s\n%s\n",
+					printHashes(stump.Roots), printPolNodes(p.roots), p.String())
+			}
+		}
+	})
+}
+
+func FuzzStumpChain(f *testing.F) {
+	var tests = []struct {
+		numAdds  uint32
+		duration uint32
+		seed     int64
+	}{
+		{3, 0x07, 0x07},
+	}
+	for _, test := range tests {
+		f.Add(test.numAdds, test.duration, test.seed)
+	}
+
+	f.Fuzz(func(t *testing.T, numAdds, duration uint32, seed int64) {
+		// simulate blocks with simchain
+		sc := newSimChainWithSeed(duration, seed)
+
+		p := NewAccumulator(true)
+		stump := Stump{}
+
+		var totalAdds, totalDels int
+		for b := 0; b <= 100; b++ {
+			adds, _, delHashes := sc.NextBlock(numAdds)
+			totalAdds += len(adds)
+			totalDels += len(delHashes)
+
+			proof, err := p.Prove(delHashes)
+			if err != nil {
+				t.Fatalf("FuzzStumpChain fail at block %d. Error: %v", b, err)
+			}
+
+			addHashes := make([]Hash, len(adds))
+			for i := range addHashes {
+				addHashes[i] = adds[i].Hash
+			}
+			stump, err = UpdateStump(delHashes, addHashes, proof, stump)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			err = p.Modify(adds, delHashes, proof.Targets)
+			if err != nil {
+				t.Fatalf("FuzzStumpChain fail at block %d. Error: %v", b, err)
+			}
+
+			// Check that the roots are the same after addition/deletion.
+			if len(p.roots) != len(stump.Roots) {
+				t.Fatalf("FuzzStumpChain fail at block %d: Have %d roots for pollard and %d roots for stump."+
+					"\nStump:\n%s\nPollard:\n%s\n", b, len(p.roots), len(stump.Roots),
+					printHashes(stump.Roots), printPolNodes(p.roots))
+			}
+			for i := range p.roots {
+				if p.roots[i].data != stump.Roots[i] {
+					t.Fatalf("FuzzStumpChain fail at block %d: Roots do not equal between pollard and stump."+
+						"\nStump:\n%s\nPollard:\n%s\n%s\n", b,
+						printHashes(stump.Roots), printPolNodes(p.roots), p.String())
+				}
+			}
+		}
+	})
+}
