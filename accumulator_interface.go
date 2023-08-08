4305bb9017ab4b1c1c30daa1847503d702eff2e7
Calvin Kim <calvin@kcalvinalvin.info>
2023-08-08 23:15:47 +0900

    utreexo: define interface for utreexo accumulators
    The Utreexo interface defines a set of methods that all utreexo
    implementations should abide by.

---
+++ accumulator_interface.go
@@ -0,0 +1,37 @@
+package utreexo
+
+// Utreexo defines the interface that all consensus abiding Utreexo implementations
+// should have.
+type Utreexo interface {
+	// Modify subtracts then adds elements to the accumulator. The deletions are done in batches
+	// so if.
+	Modify(adds []Leaf, delHashes []Hash, proof Proof) error
+
+	// Prove returns a proof of the given hashes.
+	Prove(delHashes []Hash) (Proof, error)
+
+	// Verify returns an error if the given hashes and proof hash up to a different root than
+	// the one the accumulator has.  Remember flag of true will tell the accumulator to cache
+	// the proof if it is valid.
+	Verify(delHashes []Hash, proof Proof, remember bool) error
+
+	// Undo reverts a modification done by Modify.
+	Undo(numAdds uint64, proof Proof, delHashes, prevRoots []Hash) error
+
+	// GetRoots returns the current roots of the accumulator.
+	GetRoots() []Hash
+
+	// GetHash returns the hash at the given position. Returns an empty hash if the position either
+	// doesn't exist or if the position is not cached.
+	GetHash(position uint64) Hash
+
+	// GetNumLeaves returns the number of total additions the accumulator has ever had.
+	GetNumLeaves() uint64
+
+	// GetTreeRows returns the current tree rows the accumulator has allocated for.
+	GetTreeRows() uint8
+
+	// String returns a string representation of the accumulator only if the result of GetTreeRows
+	// is less than 7. Will return the hash of roots instead if the accumulator is too tall.
+	String() string
+}
